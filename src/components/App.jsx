8f49e9e2dba5bd326d13d31752f7a21fd4bd0a96
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-04-04 10:54:59 +0200

    Added loadMore button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,39 +3,72 @@ import api from '../services/api';
 import { Searchbar } from './Searchbar';
 import { ImageGallery } from './ImageGallery';
 import { Loader } from './Loader';
+import { Button } from './Button';
 
 export class App extends Component {
   state = {
     images: [],
     isLoading: false,
     error: null,
+    page: 1,
+    perPage: 80,
+    totalPages: 0,
+    query: '',
   };
 
-  handleSubmit = async event => {
-    event.preventDefault();
+  fetchImages = async query => {
     this.setState({ isLoading: true });
-    const query = event.target.query.value;
-
     try {
-      const images = await api.fetchImagesWithQuery(query);
-      this.setState({ images });
+      const response = await api.fetchImagesWithQuery(
+        query,
+        this.state.page,
+        this.state.perPage
+      );
+      const images = response.data.hits;
+      this.setState({
+        totalPages: Math.ceil(response.data.totalHits / this.state.perPage),
+      });
+      this.setState({ images: this.state.images.concat(images) });
     } catch (error) {
       this.setState({ error });
-      console.log(error);
     } finally {
       this.setState({ isLoading: false });
     }
   };
 
+  componentDidMount = async () => {
+    await this.setState({ page: 1, query: '', images: [] });
+    this.fetchImages();
+  };
+
+  handleSearch = async event => {
+    event.preventDefault();
+    const query = event.target.query.value;
+    await this.setState({ page: 1, query, images: [] });
+    this.fetchImages(query);
+  };
+
+  handleLoadMore = async event => {
+    event.preventDefault();
+    const { query } = this.state;
+    await this.setState({ page: this.state.page + 1 });
+    this.fetchImages(query);
+  };
+
   render() {
-    const { images, isLoading, error } = this.state;
+    const { images, isLoading, error, totalPages, page } = this.state;
 
     return (
       <div>
-        <Searchbar handleSubmit={this.handleSubmit} />
+        <Searchbar handleSearch={this.handleSearch} />
         {error && <p>Whoops, something went wrong: {error.message}</p>}
         {images.length > 0 && <ImageGallery images={images} />}
-        {isLoading && <Loader />}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          images.length > 0 &&
+          totalPages > page && <Button handleLoadMore={this.handleLoadMore} />
+        )}
       </div>
     );
   }
--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 
-export const fetchImagesWithQuery = async searchQuery => {
+export const fetchImagesWithQuery = async (searchQuery, page, perPage) => {
   const response = await axios.get('https://pixabay.com/api/?', {
     params: {
       key: '34705219-9da54ac0294187b037daf6e50',
       q: searchQuery,
       image_type: 'photo',
       safesearch: true,
+      per_page: perPage,
+      page: page,
     },
   });
-  return response.data.hits;
+  return response;
 };
 
 const api = {
