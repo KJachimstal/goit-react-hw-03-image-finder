6bc5aaac4b0b605a93fe33b6ae15a12a80de258d
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-04-04 08:46:46 +0200

    Added load more button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,6 +2,13 @@ import { Component } from 'react';
 
 export class Button extends Component {
   render() {
-    return <div></div>;
+    return (
+      <button
+        className="search-form__button load-more"
+        onClick={this.props.handleLoadMore}
+      >
+        Load more
+      </button>
+    );
   }
 }
